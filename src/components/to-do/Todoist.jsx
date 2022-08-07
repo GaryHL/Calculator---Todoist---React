0c066488b7ffaec09ec6bfb7631f1aedf59d3135
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-08-07 10:09:57 +0200

    base todoistç

---
+++ src/components/to-do/Todoist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './todoist.scss'
+
+function Todoist () {
+  return (
+    <div className="list-principal-todo">
+        <h1>Mis Tareas</h1>
+    </div>
+  )
+}
+
+export default Todoist
---
+++ src/views/project_todoist/Todoist_view.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todoist from '../../components/to-do/Todoist'
+import './todoist.scss'
+function Todoist_view  (){
+  return (
+    <div className="container_todoist">
+        <Todoist/>
+    </div>
+  )
+}
+export default Todoist_view
